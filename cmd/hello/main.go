7c71a0b4c90248b833670c67111194376133751c
Thibault Ayanides <thibault.ayanides@skale-5.com>
2023-03-19 21:51:24 +0100

    feat: add logging and load conf from YAML
    closes #7

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -2,16 +2,42 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"os"
+
+	"bootstrap-golang/pkg/config"
+
+	logging "github.com/sirupsen/logrus"
 )
 
-func main() {
-	fmt.Println("Serving on http://localhost:8080 ...")
-	http.HandleFunc("/", HelloServer)
-	err := http.ListenAndServe(":8080", nil)
+var port string
+var hostname string
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	logging.SetFormatter(&logging.JSONFormatter{})
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	logging.SetOutput(os.Stdout)
+	// Only log the warning severity or above.
+	logging.SetLevel(logging.InfoLevel)
+
+	configPath := "config/configuration.yaml"
+	viper, err := config.LoadConfig(configPath)
 	if err != nil {
-		log.Fatal(err)
+		logging.Errorf("Fail to read file %s", configPath)
+	}
+
+	port = viper.GetString("port")
+	hostname = viper.GetString("hostname")
+
+}
+func main() {
+	logging.Info(fmt.Sprintf("Serving on http://%s:%s ...", hostname, port))
+	http.HandleFunc("/", HelloServer)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	if err != nil {
+		logging.Fatal(err)
 	}
 }
 
